ef43c00985036494417f133c3274d076b6d3c485
Naeem Rashid <naeemb7070@gmail.com>
2017-01-07 11:26:15 +0500

    added functionality for forward , bacward , history etc (my minor contribution :) )

---
+++ Testing/Naeem Rashid/src/controller.java
@@ -0,0 +1,14 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class controller implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
