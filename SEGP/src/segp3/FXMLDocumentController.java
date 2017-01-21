a8d95d687ead3cc4d986ba87e09333ae4c2d75c0
badar <badarshahzad54@gmail.com>
2017-01-21 13:18:35 +0500

    Right drawpane added but yet not decided final drawerpane

--- SEGP/src/segp3/FXMLDocumentController.java
+++ SEGP/src/segp3/FXMLDocumentController.java
@@ -8,6 +8,8 @@ package segp3;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.net.MalformedURLException;
@@ -34,6 +36,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
@@ -45,14 +48,17 @@ import javafx.scene.web.WebHistory.Entry;
  */
 public class FXMLDocumentController implements Initializable {
 
+	//@FXML
+	//private BorderPane borderPaneRoot;
+
 	@FXML
 	private TabPane tabpane;
 
 	@FXML
-	private JFXButton back;
+	private BorderPane borderpane;
 
 	@FXML
-	private BorderPane borderpane;
+	private JFXButton back;
 
 	@FXML
 	private JFXButton forward;
@@ -65,15 +71,13 @@ public class FXMLDocumentController implements Initializable {
 
 	@FXML
 	private Tab addNewTab;
-	
+
+	// Hamburger
 	@FXML
 	private JFXHamburger hamburger;
 
-	@FXML
-	void hamBurgerListner(MouseEvent event) {
-		
-		
-	}
+	VBox drawerPane = new VBox();
+	// JFXListView<Label> list = new JFXListView<Label>();
 
 	// Webview and browser object is going to start
 	WebView browser = new WebView();
@@ -81,7 +85,8 @@ public class FXMLDocumentController implements Initializable {
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		// TODO
+
+		// ---------------------webView---------------------------webEngine----------------------------------------------
 
 		// Default url will be google
 		webEngine.load("http://www.google.com");
@@ -107,7 +112,7 @@ public class FXMLDocumentController implements Initializable {
 			});
 
 		});
-
+		// --------------------------------------UrlField--------------------------Url--GetLocation-------------------
 		// listner for search textfield of search button
 		searchField.setOnKeyPressed(event -> {
 			if (event.getCode() == KeyCode.ENTER) {
@@ -138,45 +143,32 @@ public class FXMLDocumentController implements Initializable {
 
 			}
 		});
-	/*
-		bookmarks.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				// TODO Auto-generated method stub
-				String url = engine.getLocation();
-				System.out.println(url);
-//				engine.get
-//				write.println(url);
-			}
-			
-		});
-		History.setOnAction(new EventHandler<ActionEvent>(){
-
-			@Override
-			public void handle(ActionEvent event) {
-				// TODO Auto-generated method stub
-				 WebHistory	history = engine.getHistory();
-				 ObservableList<Entry> list = history.getEntries();
-				 for(int i=0 ; i< list.size();i++){
-					 System.out.println(list.get(i));
-				 }
-				 
-			
-			}
-			
-		});
-		
-		*/
+		/*
+		 * bookmarks.setOnAction(new EventHandler<ActionEvent>() {
+		 * 
+		 * @Override public void handle(ActionEvent event) { // TODO
+		 * Auto-generated method stub String url = engine.getLocation();
+		 * System.out.println(url); // engine.get // write.println(url); }
+		 * 
+		 * }); History.setOnAction(new EventHandler<ActionEvent>(){
+		 * 
+		 * @Override public void handle(ActionEvent event) { // TODO
+		 * Auto-generated method stub WebHistory history = engine.getHistory();
+		 * ObservableList<Entry> list = history.getEntries(); for(int i=0 ; i<
+		 * list.size();i++){ System.out.println(list.get(i)); } } });
+		 * 
+		 */
+		// --------------------------------------------------------Backward-------------------------------------------
 		back.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
 			try {
+
 				WebHistory history = webEngine.getHistory();
 				history.go(-1);
 			} catch (IndexOutOfBoundsException e1) {
 				System.out.println(e1);
 			}
 		});
-
+		// --------------------------------------------------------Forward--------------------------------------------
 		forward.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
 			try {
 				WebHistory history = webEngine.getHistory();
@@ -184,10 +176,53 @@ public class FXMLDocumentController implements Initializable {
 			} catch (IndexOutOfBoundsException e1) {
 				System.out.println(e1);
 			}
-		});		
-		
-	
-	//end method
+		});
+
+		// -------------------------------------------------------Hamburger-------------------------------------------
+
+		// Below Commited code is about the list view that will appear after
+		// hamburger click
+
+		// for(int i = 0 ; i < 6 ; i++){
+		// list.getItems().add(new Label("Item " + i));
+		// list.getStyleClass().add("mylistview");
+		// }
+		JFXButton b1 = new JFXButton("Button");
+		 drawerPane.getChildren().add(b1);
+
+		// Hamburger listner setRate 1 is active state and -1 is normal state in
+		// ham
+		HamburgerSlideCloseTransition transition = new HamburgerSlideCloseTransition(hamburger);
+		transition.setRate(-1);
+		hamburger.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+			transition.setRate(transition.getRate() * -1);
+			transition.play();
+
+			
+			 if(transition.getRate()==1){ 
+				 borderpane.setRight(drawerPane);
+				 drawerPane.setVisible(true); 
+				 } 
+			 else{ 
+				 borderpane.setRight(null);
+				 drawerPane.setVisible(false); 
+				 }
+			
+
+		});
+
+		// ------------------------------------------------------TabPane---------------------------------------------
+
+		/*
+		 * SingleSelectionModel<Tab> selectionmodel =
+		 * tabpane.getSelectionModel(); selectionmodel.select(1);
+		 * addNewTab.setClosable(false);
+		 * if(selectionmodel.getSelectedItem().getId().equals(addNewTab)){ Tab
+		 * tab = new Tab(); tab.setText("New Tab"); tabpane.getTabs().add(tab);
+		 * }
+		 */
+
+		// end method
 	}
-//end class
+	// end class
 }
